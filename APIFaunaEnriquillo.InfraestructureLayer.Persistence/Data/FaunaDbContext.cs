4ea012a863d98ab9b478e42199fdfed407148b54
YisusJ5 <mendezmedinajoseantonio210@gmail.com>
2025-05-26 14:15:07 -0400

    Agregue la configuracion del FaunaDbContext y pruebas con migrations

--- APIFaunaEnriquillo.InfraestructureLayer.Persistence/Data/FaunaDbContext.cs
+++ APIFaunaEnriquillo.InfraestructureLayer.Persistence/Data/FaunaDbContext.cs
@@ -12,8 +12,10 @@ namespace APIFaunaEnriquillo.InfrastructureLayer.Persistence.Data
     {
         public FaunaDbContext(DbContextOptions<FaunaDbContext> options) : base(options) { }
 
-        #region Modelos
+        #region Models
         DbSet<Habitat> Habitats { get; set; }
+        DbSet<Planta> Plantas { get; set; }
+        DbSet<Animal> Animals { get; set; }
         #endregion
 
         #region OnModelCreating
@@ -22,14 +24,85 @@ namespace APIFaunaEnriquillo.InfrastructureLayer.Persistence.Data
             base.OnModelCreating(modelBuilder);
             #region Tables
             modelBuilder.Entity<Habitat>().ToTable("Habitats");
+            modelBuilder.Entity<Animal>().ToTable("Animales");
+            modelBuilder.Entity<Planta>().ToTable("Plantas");
             #endregion
 
             #region Primary Key
             modelBuilder.Entity<Habitat>()
                 .HasKey(k => k.IdHabitat)
                 .HasName("PKHabitat");
+
+            modelBuilder.Entity<Animal>().
+                HasKey(k => k.IdAnimal).
+                HasName("PKAnimal");
+
+            modelBuilder.Entity<Planta>().
+                HasKey(k => k.IdPlanta).
+                HasName("PKPlanta");
+            #endregion
+
+            #region Properties
+            modelBuilder.Entity<Habitat>(entity =>
+            {
+                entity.Property(h => h.IdHabitat).IsRequired();
+                entity.Property(h => h.NombreComun).HasMaxLength(250);
+                entity.Property(h => h.NombreCientifico).HasMaxLength(250);
+                entity.Property(h => h.Descripcion).HasMaxLength(500);
+                entity.Property(h => h.UbicacionGeograficaUrl).HasMaxLength(400);
+                entity.Property(h => h.ImagenUrl).HasMaxLength(400);
+            });
+            modelBuilder.Entity<Animal>(entity =>
+            {
+                entity.Property(a => a.IdAnimal).IsRequired();
+                entity.Property(a => a.HabitatId).IsRequired();
+                entity.Property(a => a.NombreComun).HasMaxLength(250);
+                entity.Property(a => a.NombreCientifico).HasMaxLength(250);
+                entity.Property(a => a.Filo).HasMaxLength(250);
+                entity.Property(a => a.Clase);
+                entity.Property(a => a.Orden).HasMaxLength(200);
+                entity.Property(a => a.Familia).HasMaxLength(200);
+                entity.Property(a => a.Genero).HasMaxLength(100);
+                entity.Property(a => a.Especie).HasMaxLength(100);
+                entity.Property(a => a.SubEspecie).HasMaxLength(100);
+                entity.Property(a => a.Observaciones).HasMaxLength(250);
+                entity.Property(a => a.DistribucionGeograficaUrl).HasMaxLength(500);
+                entity.Property(a => a.ImagenUrl).HasMaxLength(500);
+            });
+
+            modelBuilder.Entity<Planta>(entity =>
+            {
+                entity.Property(p => p.IdPlanta).IsRequired();
+                entity.Property(p => p.HabitatId).IsRequired();
+                entity.Property(p => p.NombreComun).HasMaxLength(250);
+                entity.Property(p => p.NombreCientifico).HasMaxLength(250);
+                entity.Property(p => p.Filo).HasMaxLength(200);
+                entity.Property(p => p.Clase).HasMaxLength(200);
+                entity.Property(p => p.Orden).HasMaxLength(200);
+                entity.Property(p => p.Familia).HasMaxLength(200);
+                entity.Property(p => p.Genero).HasMaxLength(200);
+                entity.Property(p => p.Especie).HasMaxLength(200);
+                entity.Property(p => p.SubEspecie).HasMaxLength(200);
+                entity.Property(p => p.Observaciones).HasMaxLength(500);
+                entity.Property(p => p.DistribucionGeograficaUrl).HasMaxLength(500);
+                entity.Property(p => p.ImagenUrl).HasMaxLength(500);
+            });
+            #endregion
+
+            #region Relationship
+            modelBuilder.Entity<Planta>().
+                HasOne(p => p.Habitat).
+                WithMany(p => p.Plantas).
+                HasForeignKey(p => p.HabitatId);
+
+            modelBuilder.Entity<Animal>().
+                HasOne(a => a.Habitat).
+                WithMany(a => a.Animales).
+                HasForeignKey(a => a.HabitatId);
             #endregion
         }
         #endregion
+
+
     }
 }
---
+++ APIFaunaEnriquillo.InfraestructureLayer.Persistence/Migrations/20250526180941_InitBD.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIFaunaEnriquillo.InfraestructureLayer.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitBD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Habitats",
+                columns: table => new
+                {
+                    IdHabitat = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NombreComun = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    NombreCientifico = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    Clima = table.Column<int>(type: "int", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    UbicacionGeograficaUrl = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: true),
+                    ImagenUrl = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PKHabitat", x => x.IdHabitat);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animales",
+                columns: table => new
+                {
+                    IdAnimal = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NombreComun = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    NombreCientifico = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    Dieta = table.Column<int>(type: "int", nullable: false),
+                    EstadoDeConservacion = table.Column<int>(type: "int", nullable: false),
+                    FormaDeReproduccion = table.Column<int>(type: "int", nullable: false),
+                    TipoDesarrolloEmbrionario = table.Column<int>(type: "int", nullable: false),
+                    EstatusBiogeográfico = table.Column<int>(type: "int", nullable: false),
+                    Filo = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    Clase = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Orden = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Familia = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Genero = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Especie = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    SubEspecie = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Observaciones = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    DistribucionGeograficaUrl = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    ImagenUrl = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    HabitatId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PKAnimal", x => x.IdAnimal);
+                    table.ForeignKey(
+                        name: "FK_Animales_Habitats_HabitatId",
+                        column: x => x.HabitatId,
+                        principalTable: "Habitats",
+                        principalColumn: "IdHabitat",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plantas",
+                columns: table => new
+                {
+                    IdPlanta = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NombreComun = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    NombreCientifico = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    EstadoDeConservacion = table.Column<int>(type: "int", nullable: false),
+                    EstatusBiogeografico = table.Column<int>(type: "int", nullable: false),
+                    Filo = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Clase = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Orden = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Familia = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Genero = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Especie = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    SubEspecie = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Observaciones = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    DistribucionGeograficaUrl = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    ImagenUrl = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    HabitatId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PKPlanta", x => x.IdPlanta);
+                    table.ForeignKey(
+                        name: "FK_Plantas_Habitats_HabitatId",
+                        column: x => x.HabitatId,
+                        principalTable: "Habitats",
+                        principalColumn: "IdHabitat",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Animales_HabitatId",
+                table: "Animales",
+                column: "HabitatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plantas_HabitatId",
+                table: "Plantas",
+                column: "HabitatId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animales");
+
+            migrationBuilder.DropTable(
+                name: "Plantas");
+
+            migrationBuilder.DropTable(
+                name: "Habitats");
+        }
+    }
+}
